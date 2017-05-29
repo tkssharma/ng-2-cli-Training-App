f7d769af97d0c16e6380704f51481283068edc5c
Tarun Sharma <tarun.softengg@gmail.com>
2017-05-29 15:57:53 +0530

    reset password page

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+  import {localStorageService} from './../../shared/localStorage.service';
+  import {Router} from '@angular/router';
+  import {UserService} from '../user.service';
+  import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+isLoggedIn : boolean;
+myAuthForm : FormGroup;
+  constructor(private _fb:FormBuilder, private router:Router, private userService:UserService,private localStorageService : localStorageService ) {
+          this.isLoggedIn = localStorageService.get(name);
+   }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this.myAuthForm = this
+        ._fb
+        .group({
+          email: [
+            '',
+            [ < any > Validators.required, < any > Validators.minLength(3)
+            ]
+          ]
+        });
+
+      this.isLoggedIn = this.userService.isUserLoggedIn();
+      if (this.isLoggedIn) {
+        this.router.navigate(['dashboard']);
+      }
+    }
+
+    submitForm(form){
+
+    }
 
 }
