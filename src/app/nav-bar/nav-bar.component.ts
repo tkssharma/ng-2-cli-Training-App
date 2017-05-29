c6678afefb185c53a6ce2c590daf81e97a0be681
Tarun Sharma <tarun.softengg@gmail.com>
2017-05-29 15:07:09 +0530

    ng2-botstrap added

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../auth/user.service';
+import { Observable } from 'rxjs/Rx';
+import { localStorageService } from '../shared/localStorage.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  date = new Date();
+  name : string;
+  private status: {isopen: boolean} = {isopen: false};
 
-  ngOnInit() {
+  constructor(private userService: UserService, private router: Router, private localStorageService: localStorageService) {
+    this.name = localStorageService.get('name');
+    Observable.interval(1000)
+      .subscribe(() => {this.date = new Date(); });
+  }
+
+  ngOnInit() {}
+
+  logout() {
+    this.userService.logout();
   }
 
 }
